842911dbff254c400e93e43bf894ce83d4094c80
greplix <cryptonian007@protonmail.com>
2022-05-18 20:34:10 +0530

    added update func

--- ub/plugins/handlers.py
+++ ub/plugins/handlers.py
@@ -1,7 +1,16 @@
 from .. import *
 from pyrogram import Client,filters
 from pyrogram.types import Message
+from asyncio import create_subprocess_shell
+from asyncio.subprocess import PIPE
 
+
+async def runbash(cmd):
+    shell = await create_subprocess_shell(cmd,stdin=PIPE,stderr=PIPE)
+    stdout,stderr = await shell.communicate()
+    if stdout:
+        return stdout
+    return stderr
 # Some gay handlers
 
 @userbot.on_message(
@@ -42,4 +51,22 @@ async def sendHelp(client: Client,message: Message):
             query_id=results.query_id,
             result_id=results.results[0].id
     )
+
+@userbot.on_message(
+    filters.command('.update') &
+    filters.me &
+    ~filters.edited &
+    ~filters.via_bot
+)
+async def update_(c, m: Message):
+    a = await m.reply_text('**Updating Nebulus...**','md')
+    out = await runbash('git pull -f origin alpha')
+    if 'not a git repository' in out:
+        await runbash('git init . && git remote add origin https://github.com/greplix/nebulus.git && git fetch origin alpha && git checkout -f alpha')
+        out = await runbash('git pull -f origin alpha')
+    if 'Already up to date' in out:
+        await a.edit_text('`Nebulus is already up-to-date`','md')
+    else:
+        await a.edit_text(f'**Updated Nebulus**\n`{out}`')
+
 
